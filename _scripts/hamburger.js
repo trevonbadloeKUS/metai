72874d23aa478942222478f5588bb8d179ae41de
trevonbadloeKUS <trevon@korea.ac.kr>
2024-08-26 14:31:29 +0000

    Deploying to gh-pages from @ trevonbadloeKUS/trevons-lab-website@165a5e0aabc8c91da7cb2f6ba4b20f3e332fc3de 🚀

--- _scripts/hamburger.js
+++ _scripts/hamburger.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector('.hamburger');
     const navToggle = document.getElementById('nav-toggle');
+    const navMenu = document.querySelector('.nav');
+    const hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         navToggle.checked = !navToggle.checked;
+    });
+
+    // Optional: Close the menu when clicking outside of it
+    document.addEventListener('click', (event) => {
+        if (!navMenu.contains(event.target) && !hamburger.contains(event.target) && navToggle.checked) {
+            navToggle.checked = false;
+        }
     });
 });
