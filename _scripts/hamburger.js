32f18e6422287c67bffe717e9672a94c1c071677
Trevon <trevon@korea.ac.kr>
2024-08-26 21:36:49 +0900

    plesae

--- _scripts/hamburger.js
+++ _scripts/hamburger.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navToggle = document.getElementById('nav-toggle');
-    const nav = document.querySelector('.nav');
     const hamburger = document.querySelector('.hamburger');
+    const navToggle = document.getElementById('nav-toggle');
 
     hamburger.addEventListener('click', () => {
         navToggle.checked = !navToggle.checked;
